0c5eb144640ac06a6a34ec724ce0da2b21a7f269
sofo12eee <sofo12eee@users.noreply.github.com>
2026-05-13 12:59:46 +0100

    Add Supabase env variables support

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tbecimjtjtrilseatnjo.supabase.co';
-const supabaseKey = 'sb_publishable_OLA5PJO-cg43j2g0I7i0zw_xl_EqUyL';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://tbecimjtjtrilseatnjo.supabase.co';
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY || 'sb_publishable_OLA5PJO-cg43j2g0I7i0zw_xl_EqUyL';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
